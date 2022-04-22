191db49ef3f4cf7a9cb9ffabfbe487d36bb49861
Rayven Deray <rjderay@gmail.com>
2022-04-22 14:50:11 -0500

    finished upload, working on header create post

--- src/app/views/feed/create-post/create-post.component.ts
+++ src/app/views/feed/create-post/create-post.component.ts
@@ -1,28 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.scss']
+  styleUrls: ['./create-post.component.scss'],
+  providers:[
+    MatSnackBar
+  ]
 })
 export class CreatePostComponent implements OnInit {
-
-  public imageFile: File;
+  @ViewChild('uploadMoreThan1') multipleUploadTemplate: TemplateRef<any>;
+  @ViewChild('uploadIncorrectFileType') incorectTypeUploadTemplate: TemplateRef<any>;
+  public imageFile: File | null;
   public textOnly: boolean = false;
-  public imageURL: SafeUrl;
+  public imageURL: SafeUrl | null;
+  public postType: 'img' | 'txt' = 'img';
 
   constructor(
-    private _sanitizer: DomSanitizer
+    private _sanitizer: DomSanitizer,
+    private _snackbar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
   }
 
   async uploadImage(event: any){
-    this.imageFile = event.target.files[0] as File;
+    if(event.target.files.length > 1){
+      this._snackbar.openFromTemplate(this.multipleUploadTemplate, {duration: 5000});
+    }
+    const tempImageFile = event.target.files[0] as File;
+    if(!tempImageFile){
+      return;
+    }
+    if(!tempImageFile.type.includes('image')){
+      this._snackbar.openFromTemplate(this.incorectTypeUploadTemplate, {duration: 5000})
+    }
+    this.imageFile = tempImageFile;
     this.imageURL = this._sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(this.imageFile));
-    console.log(this.imageURL)
+  }
+
+  removeImage(){
+    this.imageURL = null;
+    this.imageFile = null;
   }
 
 }
